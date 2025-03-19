ae5a375958894ded3d5f86c72af2a404dee4babf
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-19 19:00:08 +0100

    [UPD] Aggiunta la stampa dell'array appena creato

--- js/script.js
+++ js/script.js
@@ -5,10 +5,14 @@ if (isNaN(n)) { // Controllo se l'utente ha inserito un numero
   alert("Non hai inserito un numero"); // Messaggio di errore}
 } else {
     for (let i = 0; i < n; i++) { // Ciclo per n volte
-    numbers.push(prompt("Inserisci un numero")); // Inserisco il numero nell'array
-    if (isNaN(numbers[i])) { // Controllo se l'utente ha inserito un numero
-      alert("Non hai inserito un numero"); // Messaggio di errore
-      break; // Esco dal ciclo
-    }
+        numbers.push(prompt("Inserisci un numero")); // Inserisco il numero nell'array
+        if (isNaN(numbers[i])) { // Controllo se l'utente ha inserito un numero
+        alert("Non hai inserito un numero"); // Messaggio di errore
+        break; // Esco dal ciclo
+        }
     }
 }
+
+console.log(numbers); // Stampo l'array
+let numbersString = numbers.join(`, `); // Creo una stringa con i numeri inseriti
+alert (`I numeri inseriti sono: ${numbersString} `); // Messaggio con i numeri inseriti
